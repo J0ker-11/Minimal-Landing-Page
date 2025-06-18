28aec4a2e81e918d1160d93ce0991300da793bce
joker-11 <primetech444@gmail.com>
2025-06-18 23:04:15 +0100

    Nav Bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,17 @@ const Header = () => {
     const listItems = ["Home", "Service","Feature", "Product", "Testimonial", "FAQ"]
     const navLinks = listItems.map((list,index) => <li key={index}>{list}</li>)
   return (
-       <header className='w-full max-w-8xl mx-auto px-4'>
+       <header className='w-full max-w-8xl mx-auto px-4 py-2'>
         <nav>
             <div className='flex justify-between items-center'>
                 {/* Nav logo */}
                 <a href="" className='text-2xl  font-semibold flex items-center space-x-3'><img src={Icon} alt="" className='w-10 inline-block items-center' /><span className=''>Nexcent</span></a>
                 
             {/* nav-links */}
-            <ul className='hidden md:flex'>
+            <ul className='hidden md:flex space-x-4'>
                 {navLinks}
             </ul>
-            <div className='border-1 border-blue-600 space-x-5 hidden lg:flex items-center max-w-[182p'>
+            <div className=' space-x-5 hidden lg:flex items-center max-w-[182px]'>
                 <a href="" className='lg:flex items-center'>Login</a>
                 <a href=""><button className='px-4 py-2 bg-green-500 text-white rounded'>Sign Up</button></a>
             </div>
